93af6756bf483b3fdc3e012fb7228540144db696
Oscar Gustafsson <oscar.gustafsson2@medieinstitutet.se>
2019-02-05 16:37:26 +0100

    modified files, not in same folder and forgot to add

--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@
 const express = require('express');
 const menu = require('./routes/menu.js');
 const orders = require('./routes/orders.js');
+const cors = require('cors');
 const port = 3000;
 let app = express();
 
 app.use(express.json());
+app.use(cors());
 
 /* This routes you around in menu */
 app.route('/menu')
