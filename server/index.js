2d9a66a16c134277ed5bb9b36f93cc1319402740
Oscar Gustafsson <oscar.gustafsson2@medieinstitutet.se>
2019-02-05 12:59:14 +0100

    second commit

--- server/index.js
+++ server/index.js
@@ -1,14 +1,21 @@
 /* For this to work, you need to run 'npm install express -g' */
 const express = require('express');
 const menu = require('./routes/menu.js');
+const orders = require('./routes/orders.js');
 const port = 3000;
 let app = express();
 
 app.use(express.json());
 
-/* This routes you around */
+/* This routes you around in menu */
 app.route('/menu')
     .get(menu.get);
+
+/* This routes you around in orders*/
+app.route('/orders')
+    .get(orders.get)
+    .post(orders.post)
+    .patch(orders.patch);
  
 /* At which port the server is on */
 app.listen(port, () => {
